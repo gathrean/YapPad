6733bf479bb6794f356c799eb5967dc8554c323e
Laurie Solkoski <lsolkoski3@my.bcit.ca>
2024-03-30 12:36:08 -0700

    created yapping chamber and set up route for it

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import LandingPage from './components/LandingPage';
 import LoginPage from './components/LoginPage';
 import SignupPage from './components/SignupPage';
 import HomePage from './components/HomePage'; 
+import Chamber from './components/Chamber';
 import AdminPage from './components/AdminPage';
 import Navbar from './components/Navbar'; 
 import Footer from './components/Footer'; 
@@ -25,15 +26,15 @@ function App() {
             <Navbar /> 
             <Routes>
                 <Route path="/" element={<LandingPage />} />
-                <Route path="/home" element={<HomePage />} /> 
+                <Route path="/home" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/signup" element={<SignupPage />} />
                 <Route path="/admin" element={<AdminPage />} />
+                <Route path="/chamber" element={<Chamber />} /> 
             </Routes>
             <Footer /> 
         </Router>
         </AuthProvider>
     );
 }
-
 export default App;
---
+++ src/components/Chamber.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Chamber() {
+  return (
+    <div>
+      <h1>Yapping Chamber</h1>
+      <p>Welcome to the Yapping Chamber. This is where all the magic happens!</p>
+    </div>
+  );
+}
+
+export default Chamber;
