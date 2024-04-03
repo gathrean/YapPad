2f03e46b80960bedeb20f8ed8583aca210b02c7c
Gathrean Dela Cruz <gathrean@icloud.com>
2024-04-03 11:46:29 -0700

    Update ReactDOM import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,11 @@
 // React and Libraries
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
 // CSS and Assets
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-
-// Function to load Swagger UI
-function loadSwaggerUI() {
-    const swaggerUIContainer = document.createElement('div');
-    swaggerUIContainer.setAttribute('id', 'swagger-ui-container');
-    document.body.appendChild(swaggerUIContainer);
-
-    // Load Swagger UI
-    const SwaggerUI = require('swagger-ui-react').default;
-    ReactDOM.render(
-        <React.StrictMode>
-            <SwaggerUI url="/docs/swagger.json" />
-        </React.StrictMode>,
-        swaggerUIContainer
-    );
-}
 
 // Render main React App
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -29,6 +13,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App />
     </React.StrictMode>
 );
-
-// Call function to load Swagger UI
-loadSwaggerUI();
