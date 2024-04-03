c406c2eff911ac0670f23fde5a4e545ffbd4c852
Gathrean Dela Cruz <gathrean@icloud.com>
2024-04-03 11:46:36 -0700

    Add Swagger documentation page

---
+++ src/pages/SwaggerDocs.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+
+function SwaggerDocs() {
+  useEffect(() => {
+    // Optional: Perform any additional initialization here
+  }, []);
+
+  return <SwaggerUI url="/API/v1/swagger.yaml" />;
+}
+
+export default SwaggerDocs;
